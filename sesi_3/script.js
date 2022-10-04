8c1ca67c0e0b0eb41fbc47b3cfcee71d5bdd0f5e
agilhz <agilhz15@gmail.com>
2022-10-04 18:58:24 +0700

    Commit finishing challenge 3

---
+++ sesi_3/script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const ul = document.querySelector('ul .menu-toggle');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+  nav.classList.toggle('slide');
+});
+
+ul.addEventListener('click', function() {
+  nav.classList.toggle('slide');
+});
+
