9d425979932b9587fc93d055008e2df4396fae30
agilhz <agilhz15@gmail.com>
2022-10-11 18:55:27 +0700

    mkdir assigment_2 + clone challange 3 untuk penyelesaian tugas

--- assigment_2/script.js
+++ assigment_2/script.js
@@ -1,6 +1,12 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const ul = document.querySelector("ul .menu-toggle");
 const nav = document.querySelector("nav ul");
+const containerImg = document.querySelector(".container-img");
+const img = document.querySelector("#photo");
+const file = document.querySelector("#file");
+const upload = document.querySelector("#upload");
+const editForm = document.querySelector(".btn-edit");
+const form = document.querySelector(".form");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
@@ -8,4 +14,34 @@ menuToggle.addEventListener("click", function () {
 
 ul.addEventListener("click", function () {
   nav.classList.toggle("slide");
+});
+
+containerImg.addEventListener("mouseenter", function () {
+  upload.style.display = "block";
+});
+
+containerImg.addEventListener("mouseleave", function () {
+  upload.style.display = "none";
+});
+
+file.addEventListener("change", function () {
+  const choosedFile = this.files[0];
+
+  if (choosedFile) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+      img.setAttribute("src", reader.result);
+    });
+
+    reader.readAsDataURL(choosedFile);
+  }
+});
+
+editForm.addEventListener("click", function () {
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
 });
